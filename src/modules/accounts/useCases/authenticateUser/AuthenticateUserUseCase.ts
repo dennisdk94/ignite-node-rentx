3109c895ca511b5f1e78f159e6ae578044f7df25
Dennis Dyodi Kawakami <denniskawakami@gmail.com>
2022-03-10 21:34:03 -0300

    criando token do usuario

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,57 @@
+import { inject, injectable } from "tsyringe";
+import { IUsersRespository } from "../../repositories/IUsersRepository";
+import { sign } from "jsonwebtoken";
+
+import { compare } from 'bcrypt'
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string,
+        email: string
+    },
+    token: string
+}
+
+@injectable()
+class AutheticateUserUseCase {
+    constructor(
+        @inject("UserRepository")
+        private usersRepository: IUsersRespository
+        ) {}
+
+    async execute({ email, password }: IRequest): Promise<IResponse> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if(!user) {
+            throw new Error("Email or password incorrect!")
+        }
+
+        const passwordMatch = await compare(password, user.password)
+
+        if(!passwordMatch) {
+            throw new Error("Email or password incorrect!")
+        }
+
+        const token = sign({}, "10042728fb4cdbeb6803a757ab9e3617", {
+            subject: user.id,
+            expiresIn: "1d"
+        })
+
+        const tokenReturn: IResponse = {
+            token,
+            user: {
+                name: user.name,
+                email: user.email
+            }
+        }
+
+        return tokenReturn
+    }
+}
+
+export { AutheticateUserUseCase }
