1ca4422c2b072580997b292c9bb5427d808d7417
Dennis Dyodi Kawakami <denniskawakami@gmail.com>
2022-03-10 23:07:08 -0300

    autenticacao nas rotas

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { UserRepository } from "../modules/accounts/repositories/implementations/UserRepository";
+import { AppError } from './../errors/AppError';
+
+interface IPayload {
+    sub: string;
+}
+
+
+export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+    const authHeader = request.headers.authorization;
+
+    if(!authHeader) {
+        throw new AppError("Token missing", 401)
+    }
+
+    const [, token] = authHeader.split(" ")
+
+   try {
+        const { sub: user_id } = verify(token, "10042728fb4cdbeb6803a757ab9e3617") as IPayload
+
+        const userRepository = new UserRepository()
+
+        const user = await userRepository.findById(user_id)
+
+        if(!user) {
+            throw new AppError("User does not exists!", 401)
+        }
+
+        next();
+   } catch {
+       throw new AppError("Invalid token", 401)
+   }
+
+}
--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from "tsyringe";
 import { hash } from 'bcrypt';
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRespository } from "../../repositories/IUsersRepository";
+import { AppError } from './../../../../errors/AppError';
 
 @injectable()
 class CreateUserUserCase {
@@ -11,7 +12,7 @@ class CreateUserUserCase {
         const userAlreadyExists = await this.usersRepository.findByEmail(email);
 
         if(userAlreadyExists) {
-            throw new Error("User already exists!");
+            throw new AppError("User already exists!");
         }
 
         const passwordHash = await hash(password, 8);
